f2d261d90debab214666c260d71a728bc2339f9c
Aryan <aryans2808@gmail.com>
2023-03-12 16:15:16 +0530

    Color coding

--- app/src/main/java/com/wcareteam/wcare/MainActivity.java
+++ app/src/main/java/com/wcareteam/wcare/MainActivity.java
@@ -2,7 +2,9 @@ package com.wcareteam.wcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(() -> {
+            //This method will be executed once the timer is over
+            // Start your app main activity
+            Intent i = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(i);
+            // close this activity
+            finish();
+        }, 1000);
     }
 }
